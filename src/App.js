3e3fc9b4876e70fd1b2858a019a47f32ea4f9cb3
cliffkang <cliffgkang@gmail.com>
2018-02-15 12:46:46 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import AppStyled from './AppStyled';
+import Character from './Character';
+import CharDetails from './CharDetails';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      starwarsChars: []
+    };
+  }
+  componentDidMount() {
+    // feel free to research what this code is doing.
+    // At a high level we are calling an API to fetch some starwars data from the open web.
+    // We then take that data and resolve it our state.
+    fetch('https://swapi.co/api/people')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ starwarsChars: data.results });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
+  render() {
+    return (
+      <AppStyled>
+        <div className='Header'></div>
+        <div className='character'>
+          {
+            this.state.starwarsChars.map(character => {
+              return <Character character={character} />
+            })
+          }
+          <ul className="char-grid">
+            {this.state.starwarsChars.map((char) => {
+              return <CharDetails char={char} />;
+            })}
+          </ul>
+        </div>
+      </AppStyled>
+    );
+  }
+}
+
+export default App;
